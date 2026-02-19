ced8cc5b2ab243c27aef366cfab3f5e9567b5c15
Anglesvar Appiya Chandrasekar <anglesvar.cheenu98@gmail.com>
2026-02-19 14:29:59 +0000

    update app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthGuardService } from './auth-guard.service';
+
 
 const routes: Routes = [
   {
     path:'',
     pathMatch:'prefix',
-    redirectTo: "login"
+    redirectTo: "dashboard"
   },
   {
     path: "login",
@@ -21,7 +21,6 @@ const routes: Routes = [
   },
   {
     path: "dashboard",
-    canActivate: [AuthGuardService],
     component: DashboardComponent,
   }
 ];
